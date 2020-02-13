175da60f53f8a2d1707ae6f2ffd79c1aca829c94
bigtonito39 <bigtonito@hotmail.com>
2020-02-12 22:22:14 -0500

    creating server example

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+// implement your API here
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req,res) =>{
+    res.send('hello world!')
+});
+
+server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+            id:1,
+            name:"jose"
+        },
+
+        {
+            id:2,
+            name: "reinoso"
+        }
+    ];
+  res.status(200).json(hobbits)
+
+})
+
+server.listen(8000, () => console.log('API Running on port 8000'))
